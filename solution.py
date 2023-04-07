8f6ec6de384bb7de49b26cf02f14dfa7610f65b1
EgorTytskyi <129987284+EgorTytskyi@users.noreply.github.com>
2023-04-07 19:44:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 628156322 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    n = len(x)
+    time = 10
+    errors = np.random.exponential(scale=1, size=n) - 21
+    v_real = x - errors
+    a = np.mean(v_real) / time
+    return a
